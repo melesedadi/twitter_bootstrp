bde1416aff255005bf81c889f3fcb6461a94817f
melesedadi <dadimeles@yahoo.com>
2019-08-22 09:43:35 -0400

    Bootstrap_502

---
+++ src/main/java/com/example/demo/SpringBoot5022Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot5022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot5022Application.class, args);
+    }
+
+}
